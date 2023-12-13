6e38a5819c9eb1eddd28696b6f31e50a093c5856
Daniel Lugo <alchem1s7@outlook.com>
2023-12-13 11:04:24 -0600

    Added descriptions to functions

--- final_format_dates.py
+++ final_format_dates.py
@@ -2,12 +2,16 @@ import pandas as pd
 
 
 def formating(df):
+        """This function prepares the date column, it has to be used previous to the 
+        function every_row. It removes undesired characters and whitespaces in the column."""
         df.reset_index(drop=True,inplace=True)
         df["Fecha"] = df["Fecha"].astype(str)
         df["Fecha"] = df["Fecha"].str.replace("00:00:00","")
         df["Fecha"] = df["Fecha"].str.strip()
         return df
 def every_row(row):
+        """This function changes the order of dates.
+        Example: Changes YYYY-mm-dd to dd-mm-YYYY"""
         lista = row.split("-")
         lista_invertida = lista[::-1]
         new_row = "-".join(lista_invertida)
